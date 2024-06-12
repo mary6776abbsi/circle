d72a1d0c1dbdba93085d98cc29be21f446ff9b87
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 18:05:23 +0330

    circle

---
+++ circle.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+const double PI = 3.14159; // تعریف ثابت پایدار برای مقدار پی
+
+// تابع برای محاسبه محیط دایره با استفاده از شعاع
+double calculatePerimeter(double radius) {
+    return 2 * PI * radius;
+}
+
+// تابع برای محاسبه مساحت دایره با استفاده از شعاع
+double calculateArea(double radius) {
+    return PI * radius * radius;
+}
+
+int main() {
+    double radius;
+
+    // دریافت شعاع از کاربر
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    // محاسبه و نمایش محیط و مساحت دایره
+    cout << "Perimeter of the circle: " << calculatePerimeter(radius) << endl;
+    cout << "Area of the circle: " << calculateArea(radius) << endl;
+
+    return 0;
+}
